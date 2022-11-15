94f81c7f961113b5e10f8019795b5f2fbff9afe3
ko-istvan <istvan.kovacs@studmail.w-hs.de>
2022-11-15 10:11:39 +0100

    implementation BufferedReader and variable n

--- Benchmark/src/main/java/de/whs/dbi/wise2223/praktikum/benchmark/Main.java
+++ Benchmark/src/main/java/de/whs/dbi/wise2223/praktikum/benchmark/Main.java
@@ -1,4 +1,14 @@
 package de.whs.dbi.wise2223.praktikum.benchmark;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Scanner BufferedReader  = new Scanner (new BufferedReader(new InputStreamReader(System.in)));
+        int n = BufferedReader.nextInt();
+    }
+
 }
