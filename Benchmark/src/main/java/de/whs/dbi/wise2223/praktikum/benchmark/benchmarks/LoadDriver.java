058d7baa707fbb2294fa894f1a2198604dde2a8f
Christopher Nattefort <chrisnatte@protonmail.com>
2022-12-06 08:40:29 +0100

    feat(transaction): insert methods

---
+++ Benchmark/src/main/java/de/whs/dbi/wise2223/praktikum/benchmark/benchmarks/LoadDriver.java
@@ -0,0 +1,6 @@
+package de.whs.dbi.wise2223.praktikum.benchmark.benchmarks;
+
+public class LoadDriver {
+
+
+}
---
+++ Benchmark/src/main/java/de/whs/dbi/wise2223/praktikum/benchmark/benchmarks/NTPSDatenbankTransaktion.java
@@ -0,0 +1,17 @@
+package de.whs.dbi.wise2223.praktikum.benchmark.benchmarks;
+
+public class NTPSDatenbankTransaktion {
+
+    public int getBalanceFromAccount(int accId) {
+        return 0;
+    }
+
+    public int updateBalance(int accId, int tellerId, int branchId, int delta) {
+        return 0;
+    }
+
+    public int getNumberOfDeltaBalance(int delta) {
+        return 0;
+    }
+    
+}
