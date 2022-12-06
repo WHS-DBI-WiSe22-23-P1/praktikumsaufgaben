68531fa73c90a8a218b95d20475fac7ba8d60931
Christopher Nattefort <chrisnatte@protonmail.com>
2022-12-06 10:20:58 +0100

    feat(drop): updating withconnection t

--- Benchmark/src/main/java/de/whs/dbi/wise2223/praktikum/benchmark/benchmarks/Drop.java
+++ Benchmark/src/main/java/de/whs/dbi/wise2223/praktikum/benchmark/benchmarks/Drop.java
@@ -11,12 +11,12 @@ public class Drop implements Benchmark {
 
     @Override
     public void run() throws SQLException {
-        takeTime("Gesamt mit Connection", () -> withConnection(connection -> {
-
-            takeTime("Gesamt ohne Connection", () -> {
-
-                connection.createStatement().execute("DROP TABLE accounts, branches, tellers, history");
-            });
-        }));
+        takeTime("Gesamt mit Connection", () ->
+                withConnection(connection -> {
+                    takeTime("Gesamt ohne Connection", () ->
+                            connection.createStatement().execute("DROP TABLE accounts, branches, tellers, history"));
+                    return null;
+                })
+        );
     }
 }
