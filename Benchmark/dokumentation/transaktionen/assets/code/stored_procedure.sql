915875d6c689c0d406601f4714f97fa897e7a689
ko-istvan <istvan.kovacs@studmail.w-hs.de>
2022-12-20 11:12:05 +0100

    doc(sp): variablennamen korrigiert

--- Benchmark/dokumentation/transaktionen/assets/code/stored_procedure.sql
+++ Benchmark/dokumentation/transaktionen/assets/code/stored_procedure.sql
@@ -1,8 +1,8 @@
 DELIMITER //
 CREATE PROCEDURE updateBalance(accountID int, tID int, brID int, delta int,commentar text)
 BEGIN
-UPDATE accounts set balance = balance + delta  WHERE accid = acID;
-UPDATE branches set balance = balance + delta  WHERE branchid = bID;
+UPDATE accounts set balance = balance + delta  WHERE accid = accountID;
+UPDATE branches set balance = balance + delta  WHERE branchid = brID;
 UPDATE tellers set balance = balance + delta  WHERE tellerid = tID;
 SELECT balance, @var_blance := balance FROM accounts WHERE accid = accountID;
 INSERT INTO history (accid, tellerid, branchid, delta,accbalance, cmmnt) VALUES (accountID, tID, brID, delta, @var_blance, commentar);
